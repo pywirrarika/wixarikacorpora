7b5a4a646a2d39f3d0f719fecc294591b294eb78
Manuel Mager <pywirrarika@gmail.com>
2017-07-19 11:30:04 -0500

    Adding scripts to work with the dhh spanish bible and with parallel corpus

--- bible/es/workon.sh
+++ bible/es/workon.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+#
+# GPL 3+ Manuel Mager
+#
+# We use dhh spanish bible. 
+# wget http://web.archive.org/web/20091026194848/geocities.com/kubyimm1/dhh-2.zip
+# wget http://web.archive.org/web/20091026194848/geocities.com/kubyimm1/dhh-2.zip
+# wget http://web.archive.org/web/20091026194848/geocities.com/kubyimm1/dhh-2.zip
+# wget http://web.archive.org/web/20091026194848/geocities.com/kubyimm1/dhh-2.zip
+#
+
+echo Cleaning dhh spanish bible...
 
 cat dhh1.txt dhh-2.txt dhh-3.txt dhh-4.txt > dhh.txt
+
 #Limpiar de notas y otras referencias
 perl -ne "print unless /\d+\. \d+\.|NOTAS:|Advertencia|http:|\([0-9\s\.\-a-zA-Z;]+\)/" dhh.txt > dhhsinnotas.txt
 tr -s "\[[:digit:]\]" " " < dhhsinnotas.txt > dhhsinnotas1.txt
 #Una línea por frase
 tr -s "[:digit:]" "\n" < dhhsinnotas1.txt > dhhlimpio.txt
 sed '/^\s*$/d' dhhlimpio.txt > dhh.txt
+
+echo Done!
